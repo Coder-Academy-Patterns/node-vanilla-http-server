49cfdcf1e8e32d3b874d3b7d65a1d8c76188eeba
Patrick Smith <patrick@burntcaramel.com>
2017-11-22 14:52:12 +1100

    Add challenges

--- readme.md
+++ readme.md
@@ -3,3 +3,17 @@
 1. Add 404 status
 1. Add image response?
 1. POST?
+
+
+## Challenges
+
+1. Add `/postcode/3021` json response
+2. Add `sendHTML(response, mainHTML)` function to **utils.js**
+    sendHTML(response, `
+    <h1>About</h1>
+    <p>This is a paragraph</p>
+    `)
+3. Change the *not found* route to send back JSON instead of plain text, while still keeping the 404 status code
+4. Add `/assets/main.css` route with some simple CSS
+5. Change `sendHTML()` to add a `<link>` to the **main.css** URL
+6. 
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const HTTP = require('http')
+const { sendJSON } = require('./utils')
 
 const server = HTTP.createServer((request, response) => {
   const path = request.url
@@ -9,8 +10,24 @@ const server = HTTP.createServer((request, response) => {
   else if (path === '/opensesame') {
     response.end('Oooh you found the secret')
   }
-  else if (path === '/about.html') {
-    response.end('<!doctype html><html><body><h1>About</h1></body></html>')
+  else if (path === '/about') {
+    sendHTML(response, `
+<h1>About</h1>
+<p>This is a paragraph</p>
+`)
+
+    response.writeHead(200, {
+      'Content-Type': 'text/html'
+    })
+    response.end(`
+<!doctype html>
+<html>
+  <body>
+    <h1>About</h1>
+    <p>This is a paragraph</p>
+  </body>
+</html>
+    `)
   }
   else if (path === '/postcode.json') {
     response.writeHead(200, {
@@ -18,8 +35,21 @@ const server = HTTP.createServer((request, response) => {
     })
     response.end(`{ "name": "Melbourne", "postcode": 3000 }`)
   }
+  else if (path === '/postcode/3020') {
+    sendJSON(response, [
+      { name: 'Albion', postcode: 3020 },
+      { name: 'Sunshine', postcode: 3020 }
+    ])
+  }
+  else if (path === '/postcode/3022') {
+    sendJSON(response, [
+      { name: 'Deer Park East', postcode: 3022 },
+      { name: 'Ardeer', postcode: 3022 }
+    ])
+  }
   else {
-    response.end('Back at you')
+    response.writeHead(404)
+    response.end('Page not found')
   }
 })
 
